816456fca9b3d444514157e399af741b716ec398
Alba <alba.talelli@mail.mcgill.ca>
2019-10-12 13:34:25 -0400

    Revert "New Branche"
    This reverts commit 16a3c6c9ef15e2a6c320e437b85c32e0c619fc84.

--- TutoringApp-Backend/src/main/java/ca/mcgill/ecse321/tutoringapp/dao/CourseRepository.java
+++ TutoringApp-Backend/src/main/java/ca/mcgill/ecse321/tutoringapp/dao/CourseRepository.java
@@ -1,13 +1,10 @@
 package ca.mcgill.ecse321.tutoringapp.dao;
 
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
 
 import ca.mcgill.ecse321.tutoringapp.model.Course;
 
 public interface CourseRepository extends CrudRepository<Course,String> {
 
-	Course findCourseByName(String CourseCode);	
-	
+	Course findCourseByName(String name);	
 }
--- TutoringApp-Backend/src/main/java/ca/mcgill/ecse321/tutoringapp/dao/SubjectRepository.java
+++ TutoringApp-Backend/src/main/java/ca/mcgill/ecse321/tutoringapp/dao/SubjectRepository.java
@@ -2,11 +2,8 @@ package ca.mcgill.ecse321.tutoringapp.dao;
 
 import org.springframework.data.repository.CrudRepository;
 
-
 import ca.mcgill.ecse321.tutoringapp.model.Subject;
 
 public interface SubjectRepository extends CrudRepository<Subject, String> {
-	
-	 Subject findSubjectByName(String name);	
 
 }
