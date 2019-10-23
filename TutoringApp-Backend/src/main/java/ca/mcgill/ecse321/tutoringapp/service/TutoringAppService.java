ab7934476ba4ec07d9c1e66c01b7d0616273289e
Alba <alba.talelli@mail.mcgill.ca>
2019-10-23 17:31:40 -0400

    Revert "Revert "Revert "Added some functions on controller"""
    This reverts commit 2ea75ac268db74534e5f204f7f6747ee0a79b35d.

--- TutoringApp-Backend/src/main/java/ca/mcgill/ecse321/tutoringapp/service/TutoringAppService.java
+++ TutoringApp-Backend/src/main/java/ca/mcgill/ecse321/tutoringapp/service/TutoringAppService.java
@@ -175,34 +175,6 @@ public class TutoringAppService {
 		return student;
 	}
 	
-	// Get Student
-	/** @author Alba Talelli */
-	public Student getStudent(String userName) {
-		String error = "";
-		if (userName == null) {
-			error = error + "Student name need to be provided";
-		} else if (!studentRepository.existsByUsername(userName)){
-			error = error + "Student does not exist!";
-		}
-		error = error.trim();
-
-		if (error.length() > 0) {
-			throw new IllegalArgumentException(error);
-		}
-		Student student = studentRepository.findByUsername(userName);
-		return student;
-	}
-	
-	// Get Tutor
-	/** @author Alba Talelli */
-	public Tutor getTutor(String userName){
-		if (userName == null) {
-			throw new IllegalArgumentException("Tutor name need to be probided");
-		}else if (!tutorRepository.existsByUsername(userName))
-			throw new IllegalArgumentException("User '" + userName + "' does not exist");
-		return tutorRepository.findTutorByUsername(userName);
-	}
-	
 	//manager
 	/**@author Helen **/
 	@Transactional
@@ -781,4 +753,7 @@ public class TutoringAppService {
 	public List<ScheduledGroupSession> getAllScheduledGroupSession(){
 		return toList(scheduledGroupSessionRepository.findAll());
 	}
+	
+
+
 }
