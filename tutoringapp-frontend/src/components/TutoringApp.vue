456626664e5e0b0ab12b49752804a0d91cf38d44
Alba <alba.talelli@mail.mcgill.ca>
2019-10-25 14:37:51 -0400

    add frontend

---
+++ tutoringapp-frontend/src/components/TutoringApp.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="tutoringapp">
+    <h2>Student</h2>
+    <table>
+      <tr>
+          <td>John</td>
+          <td>Tutoring session to attend</td>
+      </tr>
+      <tr>
+          <td>
+              <input type="text" placeholder="Student Name">
+          </td>
+          <td>
+              <button>Create</button>
+          </td>
+      </tr>
+    </table>
+    <p>
+      <span style="color:red">Error: Message text comes here</span>
+    </p>
+  </div>
+</template>
+
+<style>
+  #tutoringapp {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    color: #2c3e50;
+    background: #f2ece8;
+  }
+</style>
