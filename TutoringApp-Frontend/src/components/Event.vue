df4069ffea153cb929293ea9badc645e0d9b8285
Traian Coza <traian.coza@gmail.com>
2019-11-07 21:52:22 -0500

    Event.vue

---
+++ TutoringApp-Frontend/src/components/Event.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <table>
+            <tr v-for="person in people" >
+                <td>{{ person.name }}</td>
+            <td>
+            <ul>
+            <li v-for="event in person.events">
+            {{event.name}}
+            </li>
+            </ul>
+            </td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+    function PersonDto (name) {
+        this.name = name
+        this.events = []
+    }
+
+    function EventDto (name, date, start, end) {
+        this.name = name
+        this.eventDate = date
+        this.startTime = start
+        this.endTime = end
+    }
+
+    export default {
+        name: 'eventregistration',
+        data () {
+            return {
+                people: [],
+                newPerson: '',
+                errorPerson: '',
+                response: []
+            }
+        },
+        created: function () {
+            // Test data
+            const p1 = new PersonDto('John')
+            const p2 = new PersonDto('Jill')
+            // Sample initial content
+            this.people = [p1, p2]
+        },
+        methods: {
+            createPerson: function (personName) {
+                // Create a new person and add it to the list of people
+                var p = new PersonDto(personName)
+                this.people.push(p)
+                // Reset the name field for new people
+                this.newPerson = ''
+            }
+        }
+    }
+</script>
--- TutoringApp-Frontend/src/components/TutoringApp.vue
+++ TutoringApp-Frontend/src/components/TutoringApp.vue
@@ -1,5 +1,15 @@
 <template>
-    <div>
-        <p>Hello World!</p>
+    <div id="example-1">
+        <button v-on:click="counter += 1">Add 1</button>
+        <p>The button above has been clicked {{ counter }} times.</p>
     </div>
 </template>
+
+<script>
+    var app = new Vue({
+        el: '#example-1',
+        data: {
+            counter: 0
+        }
+    })
+</script>
