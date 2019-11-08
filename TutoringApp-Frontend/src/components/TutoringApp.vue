4e3ad807c3af446bdf7e56ef268fd932d4ebc051
Traian Coza <traian.coza@gmail.com>
2019-11-07 20:25:31 -0500

    Redid vue

--- TutoringApp-Frontend/src/components/TutoringApp.vue
+++ TutoringApp-Frontend/src/components/TutoringApp.vue
@@ -1,23 +1,5 @@
 <template>
-  <div id="tutoringapp">
-    <button v-on:click="counter += 1">Add 1</button>
-    <p>The button above has been clicked {{ counter }} times.</p>
-  </div>
+    <div>
+        <p>Hello World!</p>
+    </div>
 </template>
-
-<script>
-  new Vue({
-    el: '#tutoringapp',
-    data: {
-      counter: 0
-    }
-  });
-</script>
-
-<style>
-  #tutoringapp {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    color: #2c3e50;
-    background: #f2ece8;
-  }
-</style>
