c4a7f55dd7eb753a90f315cd3be600219f96ae1e
Traian Coza <traian.coza@gmail.com>
2019-11-09 17:05:26 -0500

    Yeha, alot of stuff works!

--- TutoringApp-Frontend/src/components/TutoringApp.vue
+++ TutoringApp-Frontend/src/components/TutoringApp.vue
@@ -2,14 +2,23 @@
   <div class="count">
     <h1>Button clicked {{ count }} times.</h1>
     <button v-on:click="count += 1">Click me!</button>
+    <button v-on:click="greet()">Greet!</button>
+    <br />
+    <br />
+    <input type='text' v-model='msg'>
   </div>
 </template>
 
 <script>
 export default {
   name: 'count',
-  data () {
-    return { count: 0 }
+  data() {
+    return { count: 0, msg: 'F' }
+  },
+  methods: {
+      greet: function () {
+          alert(this.msg);
+      }
   }
 }
 </script>
