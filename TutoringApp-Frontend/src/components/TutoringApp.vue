5c028d47cb9b6ff91d4b87d59d9bc45b269b1055
Traian Coza <traian.coza@gmail.com>
2019-11-09 16:18:15 -0500

    Got basic vue script to work!

--- TutoringApp-Frontend/src/components/TutoringApp.vue
+++ TutoringApp-Frontend/src/components/TutoringApp.vue
@@ -1,15 +1,15 @@
 <template>
-    <div id="example-1">
-        <button v-on:click="counter += 1">Add 1</button>
-        <p>The button above has been clicked {{ counter }} times.</p>
-    </div>
+  <div class="count">
+    <h1>Button clicked {{ count }} times.</h1>
+    <button v-on:click="count += 1">Click me!</button>
+  </div>
 </template>
 
 <script>
-    var app = new Vue({
-        el: '#example-1',
-        data: {
-            counter: 0
-        }
-    })
+export default {
+  name: 'count',
+  data () {
+    return { count: 0 }
+  }
+}
 </script>
