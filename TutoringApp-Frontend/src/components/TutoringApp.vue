9de57fe048c2d0c1c6f4b9856b1d8da5c0a3208b
Traian Coza <traian.coza@gmail.com>
2019-11-10 12:33:03 -0500

    Made awesome changes

--- TutoringApp-Frontend/src/components/TutoringApp.vue
+++ TutoringApp-Frontend/src/components/TutoringApp.vue
@@ -5,20 +5,35 @@
     <button v-on:click="greet()">Greet!</button>
     <br />
     <br />
-    <input type='text' v-model='msg'>
+    <ul>
+      <li v-for="message in messages" v-bind:key="message" v-on:click="msg = message">
+        <a href='#'>{{ message }}</a>
+      </li>
+    </ul>
+    <input ref='input' type='text' v-model='msg' v-on:keyup.enter='$refs.add.click(); $refs.input.select();'>
+    <button ref='add' v-on:click="addToList()">Add</button>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'count',
-  data() {
-    return { count: 0, msg: 'F' }
-  },
-  methods: {
-      greet: function () {
-          alert(this.msg);
-      }
+  export default {
+    name: 'count',
+    data() {
+      return { count: 0, msg: 'F', messages: [] }
+    },
+    methods: {
+        greet: function () {
+            alert(this.msg);
+        },
+        addToList: function () {
+            this.messages.push(this.msg);
+        }
+    }
   }
-}
 </script>
+
+<style scoped>
+  ul {
+    list-style-type: none
+  }
+</style>
