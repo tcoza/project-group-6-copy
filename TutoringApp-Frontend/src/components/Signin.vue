9a71dbadd0da93bedf70391e1cfdf9946ed7f934
Traian Coza <traian.coza@gmail.com>
2019-11-12 22:19:45 -0500

    Did nive sign in page

--- TutoringApp-Frontend/src/components/Signin.vue
+++ TutoringApp-Frontend/src/components/Signin.vue
@@ -1,11 +1,19 @@
 <template>
-    <div v-cloak>
+    <div id="signin" v-cloak>
         <div class="bar">
             <h2>Sign In</h2> 
         </div>
 
-
-
+        <br />
+        <div id="login">
+            <div id="fields">
+                Username<br />
+                <input type="text" value="Manager" /><br /><br />
+                Password<br />
+                <input type="Password" placeholder="password" /><br /><br />
+            </div>
+            <button onclick="location.href='#/home'">Sign in</button>
+        </div>
     </div>
 </template>
 
@@ -22,3 +30,18 @@
 </script>
 
 <style src="./Style.css" />
+<style scoped>
+
+#login {
+    text-align: right;
+    width: 400px;
+    margin: 0 auto;
+    padding: 10px;
+    border: 3px solid #73AD21;
+}
+
+#fields {
+    text-align: left;
+}
+
+</style>
