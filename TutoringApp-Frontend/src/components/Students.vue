da077d8770d764ae500f5b645c586e4437a2ff10
Traian Coza <traian.coza@gmail.com>
2019-11-14 17:43:53 -0500

    One small thing.

--- TutoringApp-Frontend/src/components/Students.vue
+++ TutoringApp-Frontend/src/components/Students.vue
@@ -5,13 +5,13 @@
     </div>
     <h3>Welcome, Manager!</h3>
     <div class="container">
-        <div class="scroll">
-            <table style="width: 100%"
-                ref="table"
-                tabindex="0"
-                v-on:keydown.up='$event.preventDefault(); select(selected-1)'
-                v-on:keydown.down='$event.preventDefault(); select(selected+1)'
-                v-on:keypress='search()'>
+        <div
+            tabindex="0"
+            class="scroll"
+            v-on:keydown.up='$event.preventDefault(); select(selected-1)'
+            v-on:keydown.down='$event.preventDefault(); select(selected+1)'
+            v-on:keypress='search()'>
+            <table style="width: 100%" ref="table">
                 <tr>
                     <th style="width: 25%">Username</th>
                     <th style="width: 30%">First</th>
@@ -118,6 +118,7 @@ div.scroll {
   text-align: left;
   overflow-y: auto;
   border: 3px solid #73ad21;
+  outline: none;
 }
 
 th { background-color: #4CAF50; font-size: 18pt; }
