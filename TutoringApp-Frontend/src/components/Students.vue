7b021cb3170e456e118bb8fe9b2311b806bca175
Traian Coza <traian.coza@gmail.com>
2019-11-14 16:11:32 -0500

    Did something good hopefully

--- TutoringApp-Frontend/src/components/Students.vue
+++ TutoringApp-Frontend/src/components/Students.vue
@@ -3,30 +3,41 @@
     <div class="bar">
       <h2>Students</h2>
     </div>
-    <div class="scroll">
-      <table
-      ref="table"
-      tabindex="0"
-      v-on:keydown.up='selected -= selected > 1'
-      v-on:keydown.down='selected += selected < students.length'
-      >
-        <tr>
-          <th>Username</th>
-          <th>First</th>
-          <th>Last</th>
-          <th>Status</th>
-        </tr>
-        <tr
-            v-for="index in students.length"
-            v-bind:key="index"
-            v-on:mousedown="select(index)"
-            v-bind:class="[selected == index ? 'highlight' : '']">
-          <td class="username">{{ students[index-1].username }}</td>
-          <td>{{ students[index-1].firstname }}</td>
-          <td>{{ students[index-1].lastname }}</td>
-          <td class="status">{{ students[index-1].status }}</td>
-        </tr>
-      </table>
+    <div class="container">
+        <input list="browsers">
+        <datalist id="browsers">
+            <option v-for="student in students" v-bind:key="student.username" v-bind:value="student.username" />
+        </datalist>
+        <button v-on:click="remove()">Remove</button>
+        <div class="scroll">
+            <table style="width: 100%"
+                ref="table"
+                tabindex="0"
+                v-on:keydown.up='selected -= selected > 1'
+                v-on:keydown.down='selected += selected < students.length'>
+                <tr>
+                    <th style="width: 25%">Username</th>
+                    <th style="width: 30%">First</th>
+                    <th style="width: 30%">Last</th>
+                    <th style="width: auto">Status</th>
+                </tr>
+                <tr
+                    v-for="index in students.length"
+                    v-bind:key="index"
+                    v-on:mousedown="select(index)"
+                    v-bind:class="[selected == index ? 'highlight' : '']">
+                <td>{{ students[index-1].username }}</td>
+                <td>{{ students[index-1].firstname }}</td>
+                <td>{{ students[index-1].lastname }}</td>
+                <td>
+                    <select v-bind:value='students[index-1].status'>
+                        <option value="ACTIVE">Active</option>
+                        <option value="REMOVED">Removed</option>
+                    </select>
+                </td>
+                </tr>
+            </table>
+        </div>
     </div>
   </div>
 </template>
@@ -40,27 +51,9 @@ export default {
     },
     created: function()
     {
-        this.students.push({ username: 'traian', firstname: 'Traian', lastname: 'Coza', status: 'PENDING' });
-        this.students.push({ username: 'arianit', firstname: 'Arianit', lastname: 'Vavla', status: 'PENDING' });
-        this.students.push({ username: 'odero', firstname: 'Odero', lastname: 'Otieno', status: 'PENDING' });
-        this.students.push({ username: 'traian', firstname: 'Traian', lastname: 'Coza', status: 'PENDING' });
-        this.students.push({ username: 'arianit', firstname: 'Arianit', lastname: 'Vavla', status: 'PENDING' });
-        this.students.push({ username: 'odero', firstname: 'Odero', lastname: 'Otieno', status: 'PENDING' });
-        this.students.push({ username: 'traian', firstname: 'Traian', lastname: 'Coza', status: 'PENDING' });
-        this.students.push({ username: 'arianit', firstname: 'Arianit', lastname: 'Vavla', status: 'PENDING' });
-        this.students.push({ username: 'odero', firstname: 'Odero', lastname: 'Otieno', status: 'PENDING' });
-        this.students.push({ username: 'traian', firstname: 'Traian', lastname: 'Coza', status: 'PENDING' });
-        this.students.push({ username: 'arianit', firstname: 'Arianit', lastname: 'Vavla', status: 'PENDING' });
-        this.students.push({ username: 'odero', firstname: 'Odero', lastname: 'Otieno', status: 'PENDING' });
-        this.students.push({ username: 'traian', firstname: 'Traian', lastname: 'Coza', status: 'PENDING' });
-        this.students.push({ username: 'arianit', firstname: 'Arianit', lastname: 'Vavla', status: 'PENDING' });
-        this.students.push({ username: 'odero', firstname: 'Odero', lastname: 'Otieno', status: 'PENDING' });
-        this.students.push({ username: 'traian', firstname: 'Traian', lastname: 'Coza', status: 'PENDING' });
-        this.students.push({ username: 'arianit', firstname: 'Arianit', lastname: 'Vavla', status: 'PENDING' });
-        this.students.push({ username: 'odero', firstname: 'Odero', lastname: 'Otieno', status: 'PENDING' });
-        this.students.push({ username: 'traian', firstname: 'Traian', lastname: 'Coza', status: 'PENDING' });
-        this.students.push({ username: 'arianit', firstname: 'Arianit', lastname: 'Vavla', status: 'PENDING' });
-        this.students.push({ username: 'odero', firstname: 'Odero', lastname: 'Otieno', status: 'PENDING' });
+        this.students.push({ username: 'traian', firstname: 'Traian', lastname: 'Coza', status: 'ACTIVE' });
+        this.students.push({ username: 'arianit', firstname: 'Arianit', lastname: 'Vavla', status: 'ACTIVE' });
+        this.students.push({ username: 'odero', firstname: 'Odero', lastname: 'Otieno', status: 'REMOVED' });
     },
     mounted: function()
     {
@@ -83,28 +76,24 @@ export default {
 
 <style src="./Style.css" />
 <style scoped>
-div.scroll {
-  margin: auto;
-  padding: 4px;
+div.container {
   width: 1000px;
+  text-align: right;
+}
+
+div.scroll {
+  margin-top: 20px;
   height: 500px;
-  overflow-x: auto;
+  padding: 5px;
+  overflow-y: auto;
   text-align: justify;
   border: 3px solid #73ad21;
 }
-th {
-    font-size: 18pt;
-}
-td {
-    width: 350px;
-    font-size: 16pt;
-}
-td.username {
-    width: 200px;
-}
-td.status {
-    width: 100px;
-}
+
+tr { margin-top: 10px; }
+th { background-color: #4CAF50; font-size: 18pt; }
+td { font-size: 16pt; }
+
 .highlight {
     background-color: greenyellow;
 }
