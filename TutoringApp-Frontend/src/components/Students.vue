5de627f82b2f22ed9a4f7ad314257d99ff58d6e6
Traian Coza <traian.coza@gmail.com>
2019-11-14 16:39:44 -0500

    Lookin good

--- TutoringApp-Frontend/src/components/Students.vue
+++ TutoringApp-Frontend/src/components/Students.vue
@@ -3,41 +3,35 @@
     <div class="bar">
       <h2>Students</h2>
     </div>
-    <div class="container">
-        <input list="browsers">
-        <datalist id="browsers">
-            <option v-for="student in students" v-bind:key="student.username" v-bind:value="student.username" />
-        </datalist>
-        <button v-on:click="remove()">Remove</button>
-        <div class="scroll">
-            <table style="width: 100%"
-                ref="table"
-                tabindex="0"
-                v-on:keydown.up='selected -= selected > 1'
-                v-on:keydown.down='selected += selected < students.length'>
-                <tr>
-                    <th style="width: 25%">Username</th>
-                    <th style="width: 30%">First</th>
-                    <th style="width: 30%">Last</th>
-                    <th style="width: auto">Status</th>
-                </tr>
-                <tr
-                    v-for="index in students.length"
-                    v-bind:key="index"
-                    v-on:mousedown="select(index)"
-                    v-bind:class="[selected == index ? 'highlight' : '']">
+    <h3>Welcome, Manager!</h3>
+    <div class="scroll">
+        <table style="width: 100%"
+            ref="table"
+            v-on:keydown.up='$event.preventDefault(); select(selected-1)'
+            v-on:keydown.down='$event.preventDefault(); select(selected+1)'>
+            <tr>
+                <th style="width: 25%">Username</th>
+                <th style="width: 30%">First</th>
+                <th style="width: 30%">Last</th>
+                <th style="width: auto">Status</th>
+            </tr>
+            <tr
+                v-for="index in students.length"
+                v-bind:key="index"
+                v-bind:tabindex="index"
+                v-on:mousedown="select(index)"
+                v-bind:class="[selected == index ? 'highlight' : '']">
                 <td>{{ students[index-1].username }}</td>
                 <td>{{ students[index-1].firstname }}</td>
                 <td>{{ students[index-1].lastname }}</td>
                 <td>
-                    <select v-bind:value='students[index-1].status'>
+                    <select v-model='students[index-1].status'>
                         <option value="ACTIVE">Active</option>
                         <option value="REMOVED">Removed</option>
                     </select>
                 </td>
-                </tr>
-            </table>
-        </div>
+            </tr>
+        </table>
     </div>
   </div>
 </template>
@@ -54,6 +48,42 @@ export default {
         this.students.push({ username: 'traian', firstname: 'Traian', lastname: 'Coza', status: 'ACTIVE' });
         this.students.push({ username: 'arianit', firstname: 'Arianit', lastname: 'Vavla', status: 'ACTIVE' });
         this.students.push({ username: 'odero', firstname: 'Odero', lastname: 'Otieno', status: 'REMOVED' });
+         this.students.push({ username: 'traian', firstname: 'Traian', lastname: 'Coza', status: 'ACTIVE' });
+        this.students.push({ username: 'arianit', firstname: 'Arianit', lastname: 'Vavla', status: 'ACTIVE' });
+        this.students.push({ username: 'odero', firstname: 'Odero', lastname: 'Otieno', status: 'REMOVED' });
+         this.students.push({ username: 'traian', firstname: 'Traian', lastname: 'Coza', status: 'ACTIVE' });
+        this.students.push({ username: 'arianit', firstname: 'Arianit', lastname: 'Vavla', status: 'ACTIVE' });
+        this.students.push({ username: 'odero', firstname: 'Odero', lastname: 'Otieno', status: 'REMOVED' });
+         this.students.push({ username: 'traian', firstname: 'Traian', lastname: 'Coza', status: 'ACTIVE' });
+        this.students.push({ username: 'arianit', firstname: 'Arianit', lastname: 'Vavla', status: 'ACTIVE' });
+        this.students.push({ username: 'odero', firstname: 'Odero', lastname: 'Otieno', status: 'REMOVED' });
+         this.students.push({ username: 'traian', firstname: 'Traian', lastname: 'Coza', status: 'ACTIVE' });
+        this.students.push({ username: 'arianit', firstname: 'Arianit', lastname: 'Vavla', status: 'ACTIVE' });
+        this.students.push({ username: 'odero', firstname: 'Odero', lastname: 'Otieno', status: 'REMOVED' });
+         this.students.push({ username: 'traian', firstname: 'Traian', lastname: 'Coza', status: 'ACTIVE' });
+        this.students.push({ username: 'arianit', firstname: 'Arianit', lastname: 'Vavla', status: 'ACTIVE' });
+        this.students.push({ username: 'odero', firstname: 'Odero', lastname: 'Otieno', status: 'REMOVED' });
+         this.students.push({ username: 'traian', firstname: 'Traian', lastname: 'Coza', status: 'ACTIVE' });
+        this.students.push({ username: 'arianit', firstname: 'Arianit', lastname: 'Vavla', status: 'ACTIVE' });
+        this.students.push({ username: 'odero', firstname: 'Odero', lastname: 'Otieno', status: 'REMOVED' });
+         this.students.push({ username: 'traian', firstname: 'Traian', lastname: 'Coza', status: 'ACTIVE' });
+        this.students.push({ username: 'arianit', firstname: 'Arianit', lastname: 'Vavla', status: 'ACTIVE' });
+        this.students.push({ username: 'odero', firstname: 'Odero', lastname: 'Otieno', status: 'REMOVED' });
+         this.students.push({ username: 'traian', firstname: 'Traian', lastname: 'Coza', status: 'ACTIVE' });
+        this.students.push({ username: 'arianit', firstname: 'Arianit', lastname: 'Vavla', status: 'ACTIVE' });
+        this.students.push({ username: 'odero', firstname: 'Odero', lastname: 'Otieno', status: 'REMOVED' });
+         this.students.push({ username: 'traian', firstname: 'Traian', lastname: 'Coza', status: 'ACTIVE' });
+        this.students.push({ username: 'arianit', firstname: 'Arianit', lastname: 'Vavla', status: 'ACTIVE' });
+        this.students.push({ username: 'odero', firstname: 'Odero', lastname: 'Otieno', status: 'REMOVED' });
+         this.students.push({ username: 'traian', firstname: 'Traian', lastname: 'Coza', status: 'ACTIVE' });
+        this.students.push({ username: 'arianit', firstname: 'Arianit', lastname: 'Vavla', status: 'ACTIVE' });
+        this.students.push({ username: 'odero', firstname: 'Odero', lastname: 'Otieno', status: 'REMOVED' });
+         this.students.push({ username: 'traian', firstname: 'Traian', lastname: 'Coza', status: 'ACTIVE' });
+        this.students.push({ username: 'arianit', firstname: 'Arianit', lastname: 'Vavla', status: 'ACTIVE' });
+        this.students.push({ username: 'odero', firstname: 'Odero', lastname: 'Otieno', status: 'REMOVED' });
+         this.students.push({ username: 'traian', firstname: 'Traian', lastname: 'Coza', status: 'ACTIVE' });
+        this.students.push({ username: 'arianit', firstname: 'Arianit', lastname: 'Vavla', status: 'ACTIVE' });
+        this.students.push({ username: 'odero', firstname: 'Odero', lastname: 'Otieno', status: 'REMOVED' });
     },
     mounted: function()
     {
@@ -63,8 +93,10 @@ export default {
     {
         select: function(index)
         {
+            if (index < 1 || index > this.students.length)
+                return;
             this.selected = index;
-            this.$refs.table.focus();
+            this.$refs.table.rows[index].focus();
         },
         popup: function(message)
         {
@@ -76,21 +108,18 @@ export default {
 
 <style src="./Style.css" />
 <style scoped>
-div.container {
-  width: 1000px;
-  text-align: right;
-}
 
 div.scroll {
   margin-top: 20px;
+  margin: auto;
   height: 500px;
+  width: 1000px;
   padding: 5px;
   overflow-y: auto;
   text-align: justify;
   border: 3px solid #73ad21;
 }
 
-tr { margin-top: 10px; }
 th { background-color: #4CAF50; font-size: 18pt; }
 td { font-size: 16pt; }
 
