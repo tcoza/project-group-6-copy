7176cc49a21a75d19be783b5264e38f5825348b3
Traian Coza <traian.coza@gmail.com>
2019-11-17 15:44:51 -0500

    It compiles now

--- TutoringApp-Frontend/src/components/CourseSubjectSchool.vue
+++ TutoringApp-Frontend/src/components/CourseSubjectSchool.vue
@@ -113,7 +113,7 @@
         },
         add() {
             this.originalData = null
-            this.items.push({ Teaching institution: ''})
+            this.items.push({ institution: ''})
             this.editIndex = this.items.length - 1
             this.originalData = null
             this.editIndex = null
