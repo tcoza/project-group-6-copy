95936f2bc22349795c8ea35b26b06cdfd192dc21
Alba <alba.talelli@mail.mcgill.ca>
2019-11-17 23:24:27 -0500

    evaluation done i think

--- TutoringApp-Frontend/src/components/Evaluations.vue
+++ TutoringApp-Frontend/src/components/Evaluations.vue
@@ -1,26 +1,182 @@
 <template>
-    <div id="main" v-cloak>
-        <div class="bar">
-            <h2> TutoringApp Manager View - Courses, Subjects, and Teaching Institutions </h2> 
-        </div>
-            <h3> Welcome, Manager (name)! </h3>
-        <div class ="tables">
-            <table class ="tutor" style="width: 100%" ref="table">
-                <tr>
-                    <th style="width: 30%">Student Name </th>
-                    <th style="width: 20%"> Number of evaluations</th>
-                </tr>
-            </table>
-
-            <table class= "student" style="width: 100%" ref="table">
-                <tr>
-                    <th style="width: 30%">Student Name </th>
-                    <th style="width: 20%"> Number of evaluations</th>
-                </tr>
-            </table>
-        </div>
+  <div class="eval">
+    <div class="bar">
+      <h2>Evaluations</h2>
     </div>
+    <h3>Welcome, Manager!</h3>
+    <div>
+    <div class = "title">
+        <h4> Students </h4>
+        <h4> Tutors </h4>
+    </div>
+        <div 
+            tabindex="0"
+            ref="scroll"
+            class="scroll"
+            v-on:keydown.up='$event.preventDefault(); select(selected-1)'
+            v-on:keydown.down='$event.preventDefault(); select(selected+1)'
+            v-on:keypress='search()'>
+            <table class= "TableS" style="width: 50%" ref="table">
+                <tr>
+                    <th style="width: 10%">First</th>
+                    <th style="width: 10%">Last</th>
+                    <th style="width: 10%">Grade</th>
+                    <th style="width: auto">Comment</th>
+                    <th style="width: 10%">Status</th>
+                </tr>
+                <tr
+                    v-for="index in students.length"
+                    v-bind:key="index"
+                    v-bind:tabindex="index"
+                    v-on:mousedown="select(index)"
+                    v-bind:class="[selected == index ? 'highlight' : '']">
+                    <td>{{ students[index-1].firstName }}</td>
+                    <td>{{ students[index-1].lastName }}</td>
+                    <td>{{ students[index-1].grade }}</td>
+                    <td>{{ students[index-1].comment }}</td>
+                    <td>
+                        <select
+                        v-model='students[index-1].isActiveAccount'
+                        v-on:change="statusChangedS(index-1)">
+                            <option value="true">Accept</option>
+                            <option value="false">Removed</option>
+                        </select>
+                    </td>
+                </tr>
+            </table>
+            <table class="tableT" style="width: 50%" ref="table">
+                <tr>
+                    <th style="width: 10%">First</th>
+                    <th style="width: 10%">Last</th>
+                    <th style="width: 10%">Grade</th>
+                    <th style="width: auto">Comment</th>
+                    <th style="width: 10%">Status</th>
+                </tr>
+                <tr
+                    v-for="index in tutors.length"
+                    v-bind:key="index"
+                    v-bind:tabindex="index"
+                    v-on:mousedown="select(index)"
+                    v-bind:class="[selected == index ? 'highlight' : '']">
+                    <td>{{ tutors[index-1].firstName }}</td>
+                    <td>{{ tutors[index-1].lastName }}</td>
+                    <td>{{ tutors[index-1].grade }}</td>
+                    <td>{{ tutors[index-1].comment }}</td>
+                    <td>
+                        <select
+                        v-model='tutors[index-1].isActiveAccount'
+                        v-on:change="statusChangedS(index-1)">
+                            <option value="true">Accept</option>
+                            <option value="false">Removed</option>
+                        </select>
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <input
+        ref="searchbox"
+        style="display: none"
+        v-model="query"
+        v-on:keydown.esc="$refs.scroll.focus()"
+        v-on:focusout="unsearch()" />
+    </div>
+  </div>
 </template>
 <script>
+export default {
+    name: "students",
+    name: "tutors",
+    data: function() {
+        return { students: [], selected: undefined, query: undefined, tutors: [] };
+    },
+    created: function()
+    {
+        const userAction = async () => {
+            const response = await fetch('http://localhost:8080/createstudentevaluation');
+            var myJson = await response.json(); //extract JSON from the http response
+            // do something with myJson
+            this.students = myJson._embedded.students
+            this.students.forEach((student) => student.username = student._links.self.href.substr(student._links.self.href.lastIndexOf('/')+1));
+            this.students.sort((a,b) => (a.username > b.username) ? 1 : -1);
+
+            const response1 = await fetch('http://localhost:8080/createtutorevaluation');
+            myJson = await response1.json(); //extract JSON from the http response
+            // do something with myJson
+            this.tutors = myJson._embedded.tutors
+            this.tutors.forEach((tutor) => tutor.username = tutor._links.self.href.substr(student._links.self.href.lastIndexOf('/')+1));
+            this.tutor.sort((a,b) => (a.username > b.username) ? 1 : -1);
+        }
+        userAction();
+    },
+    methods:
+    {
+        select: function(index)
+        {
+            if (index < 1 || index > this.students.length)
+                return;
+            this.selected = index;
+            this.$refs.table.rows[index].focus();
+        },
+        search: function (query)
+        {
+            this.$refs.searchbox.style.display = "initial";
+            this.query = query;
+            this.$refs.searchbox.focus();
+        },
+        unsearch: function()
+        {
+            this.$refs.searchbox.style.display = "none";
+        },
+        statusChangedS(index)
+        {
+            var url = 'http://localhost:8080/createstudentevaluation/' + this.students[index].username;
+            url += this.students[index].isActiveAccount == "true" ? '/reactivate' : '/deactivate';
+
+            const userAction = async () => {
+                const response = await fetch(url, { method: "POST" });
+                if (!response.ok)
+                    console.log(response);
+            }
+            userAction();
+        },
+        statusChangedT(index)
+        {
+            var url = 'http://localhost:8080/createtutorevaluation/' + this.tutors[index].username;
+            url += this.tutors[index].isActiveAccount == "true" ? '/reactivate' : '/deactivate';
+
+            const userAction = async () => {
+                const response = await fetch(url, { method: "POST" });
+                if (!response.ok)
+                    console.log(response);
+            }
+            userAction();
+        },
+        popup: function(message)
+        {
+            console.log(message);
+        }
+    }
+}
 </script>
+
 <style src="./Style.css" />
+<style scoped>
+
+.scroll{
+    display:flex;
+}
+
+.title{
+    display:flex;
+}
+h4{
+    font-size: 30px;
+    margin-right: 530px;
+    font-weight: bold;
+    text-align: left;
+    color: #4CAF50;
+    font-family:Times;
+}
+
+
+</style>
